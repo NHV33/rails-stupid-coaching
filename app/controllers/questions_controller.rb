2b6adf1377aa14effe3af0caf9e649eef10766ec
Noel Vincent <noelvincent@me.com>
2024-01-02 19:51:23 +0900

    rails-stupid-coaching complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,8 +3,16 @@ class QuestionsController < ApplicationController
   def ask
     @test = "please display"
   end
+
   def answer
-    @answer = "does this work?"
-    @answer = params[:answer]
+    @question = params[:question]
+    # To do: check input using regex
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.chars.last == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
